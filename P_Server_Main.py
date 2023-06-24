44add1d6b0d36a591329482672e794c8c5634651
SungJungHo1 <s214q@naver.com>
2023-06-24 17:35:35 +0900

    sdsd

--- P_Server_Main.py
+++ P_Server_Main.py
@@ -1,1 +1,17 @@
-print("Hi")
+from fastapi import FastAPI, File, Form, UploadFile, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post('/refund')
+async def PostRefund():
+    print("")
+    return ""
